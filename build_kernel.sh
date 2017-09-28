6eac93866a29105134e697ff9a0611ddd32913de
Victor Lourme <contact@zeroside.co>
2017-09-28 18:03:02 +0200

    build: edit automatic building script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-#rm log.txt
-#make clean
-#make distclean
-#export CROSS_COMPILE=/home/tien.tq1/toolchains/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
 export ARCH=arm64
 
-make exynos7580-a5xelte_defconfig
-make -j
-#24 2>&1 | tee -a  log.txt
+if [ ! -d $(pwd)/output ];
+    then
+        mkdir $(pwd)/output;
+    fi
+
+make -C $(pwd) O=output ARCH=arm64 "lineage-a5xeltexx_defconfig"
+make -j8 -C $(pwd) O=output ARCH=arm64
+
+exit
